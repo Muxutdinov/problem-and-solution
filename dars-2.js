ce364824d90151ddd02ff1321fd110ed0060d0b2
Muxutdinov <jmuxutdinov@mail.ru>
2021-12-20 10:44:20 +0500

    task ikki

--- dars-2.js
+++ dars-2.js
@@ -2,12 +2,24 @@
 // Tezlik cheklovi => 70 km/soat
 // point => guvohnoma olib qoyiladi
 
+
+const SPEEDLIMIT = 70
+const MAXPOINTS = 12
+const POINT  = 5
+
 function checkSpeed(speed){
-if(speed <= 70){
-    console.log(Math.floor(speed));
+if(speed < SPEEDLIMIT + POINT){
+    console.log("OK");
+}
+else{
+    const Points = Math.floor((speed - SPEEDLIMIT) / POINT);
+    if( Points >= MAXPOINTS ){
+        console.log("Guvohnoma olib qo'yilsin");
+    }
+    else{
+        console.log(Points);
+    }
 }
 }
 
-checkSpeed(24.43)
-
-
+checkSpeed(75)

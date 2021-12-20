bc99c0ef721b99fa1301ae30b7984013b714c82e
Muxutdinov <jmuxutdinov@mail.ru>
2021-12-20 09:11:45 +0500

    second task started

--- dars-1.js
+++ dars-1.js
@@ -1,4 +1,19 @@
-console.log("Jaxongir");
-
-
-
+function FizzBuzz(input){
+if(typeof input !== "number"){
+    return "son emas!"
+}
+else if((input%5===0) && (input%5===0)){
+    return "FizzBuzz"
+}
+else if(input%3===0){
+    return "Fizz"
+}
+else if( input % 5 === 0 ) {
+ return "Buzz"
+}
+else{
+    return input
+}
+}
+let natija =  FizzBuzz(8)
+console.log(natija);
---
+++ dars-2.js
@@ -0,0 +1,13 @@
+// ===========  Task
+// Tezlik cheklovi => 70 km/soat
+// point => guvohnoma olib qoyiladi
+
+function checkSpeed(speed){
+if(speed <= 70){
+    console.log(Math.floor(speed));
+}
+}
+
+checkSpeed(24.43)
+
+
